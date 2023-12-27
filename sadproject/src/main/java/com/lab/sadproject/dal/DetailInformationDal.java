96b0270f27365c02feadfdedfe570e8334b030aa
seckin <seckinkurtalan02@gmail.com>
2023-12-27 20:35:50 +0300

    com

--- sadproject/src/main/java/com/lab/sadproject/dal/DetailInformationDal.java
+++
@@ -1,7 +0,0 @@
-package com.lab.sadproject.dal;
-
-import com.lab.sadproject.entity.DetailInformation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetailInformationDal extends JpaRepository<DetailInformation, String> {
-}
